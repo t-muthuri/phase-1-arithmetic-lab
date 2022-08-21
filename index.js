023714176a1855847dd4731c473a696e86b9f69c
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-08-21 21:39:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = (num1*num2);
 
-// Write your code here
+
+const random = (3, 18, 4, 7, 20);
+Math.max (random);
+
+const num3 = 32;
+const num4 = 7;
+const mod = (num3%num4)
+
+
+const max = (1, 20);
+Math.max (max);
